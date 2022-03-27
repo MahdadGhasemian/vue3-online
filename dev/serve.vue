beca1eb60a4424d7e2710e7170d81c0fc3abbcb3
mahdad ghasemian <mahdad.ghasemian@gmail.com>
2022-03-27 16:20:07 +0430

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import Vue3Online from '@/vue3-online.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3Online
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-online />
+  </div>
+</template>
---
+++ src/vue3-online.vue
@@ -0,0 +1,41 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Vue3Online",
+  data() {
+    return {
+      onlineStatus: navigator.onLine,
+    };
+  },
+  mounted() {
+    this.addHandler();
+  },
+  unmounted() {
+    this.remveHanlder();
+  },
+  methods: {
+    addHandler() {
+      window.addEventListener("online", () => {
+        this.onlineStatus = true;
+      });
+      window.addEventListener("offline", () => {
+        this.onlineStatus = false;
+      });
+    },
+    remveHanlder() {
+      window.removeEventListener("online", () => {});
+      window.removeEventListener("offline", () => {});
+    },
+  },
+  computed: {
+    isOnline() {
+      return this.onlineStatus;
+    },
+  },
+});
+</script>
+
+<template>
+  <slot v-if="isOnline"></slot>
+</template>
